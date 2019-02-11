6b64a878fa6f149e6fdea8d8ff56bc9651c31e7a
joaov777 <joaov777@gmail.com>
2019-02-11 08:02:04 -0300

    Improving mainconf and adjusting last scripts - 01

--- mainconf.sh
+++ mainconf.sh
@@ -20,16 +20,16 @@
 
 
     case $menuChoice in
-        1) ~/mainconf/mainconftree/setupgituser.sh ;;
-        2) ~/mainconf/mainconftree/installzsh.sh ;;
-        3) ~/mainconf/mainconftree/updatezshrc.sh ;;
-        4) ~/mainconf/mainconftree/updatesystem.sh ;;
-        5) ~/mainconf/mainconftree/updatepackages.sh ;;
-        6) ~/mainconf/mainconftree/updatetilda.sh ;;
-        7) ~/mainconf/mainconftree/checkinstalledpackages.sh ;;
-	    8) ~/mainconf/mainconftree/setupplank.sh ;;
-        9) ~/mainconf/mainconftree/exitmainconf.sh ; exit ;;
-        *) ~/mainconf/mainconftree/invalidoption.sh ;;
+            1) ~/mainconf/mainconftree/setupgituser.sh ;;
+            2) ~/mainconf/mainconftree/installzsh.sh ;;
+            3) ~/mainconf/mainconftree/updatezshrc.sh ;;
+            4) ~/mainconf/mainconftree/updatesystem.sh ;;
+            5) ~/mainconf/mainconftree/updatepackages.sh ;;
+            6) ~/mainconf/mainconftree/updatetilda.sh ;;
+            7) ~/mainconf/mainconftree/checkinstalledpackages.sh ;;
+	        8) ~/mainconf/mainconftree/setupplank.sh ;;
+            9) ~/mainconf/mainconftree/exitmainconf.sh ; exit ;;
+            *) ~/mainconf/mainconftree/invalidoption.sh ;;
     esac
 
     done
