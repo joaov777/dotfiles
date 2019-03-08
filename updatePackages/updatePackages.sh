53d2dfb41aa321989d4eab50781e8933a795ce8c
João Victor <39555841+joaov777@users.noreply.github.com>
2019-03-08 06:58:21 -0300

    Update updatePackages.sh

--- updatePackages/updatePackages.sh
+++ updatePackages/updatePackages.sh
@@ -7,22 +7,22 @@
 	trizen -S notepadqq --noconfirm
 	trizen -S gparted --noconfirm
 	trizen -S keeweb-desktop --noconfirm
-	trizen -S ipscan --noconfirm 
-	trizen -S tilda --noconfirm
-	trizen -S upterm --noconfirm
-	trizen -S obs-studio --noconfirm
+	#trizen -S ipscan --noconfirm 
+	#trizen -S tilda --noconfirm
+	#trizen -S upterm --noconfirm
+	#trizen -S obs-studio --noconfirm
 	trizen -S filezilla --noconfirm
-	trizen -S gnome-pie --noconfirm
+	#trizen -S gnome-pie --noconfirm
 	trizen -S telegram-desktop --noconfirm	
 	trizen -S google-chrome --noconfirm
-	trizen -S veracrypt --noconfirm
+	#trizen -S veracrypt --noconfirm
 	trizen -S ranger --noconfirm
 	trizen -S parcellite --noconfirm
 	trizen -S flameshot --noconfirm
 	trizen -S guake --noconfirm
 	trizen -S remmina --noconfirm
 	#trizen -S skype-desktop-bin --noconfirm
-	trizen -S skypeforlinux-preview-bin --noconfirm
+	#trizen -S skypeforlinux-preview-bin --noconfirm
 	trizen -S simplenote-electron-bin --noconfirm
 	trizen -S gedit --noconfirm
 	trizen -S barrier --noconfirm	
@@ -31,28 +31,28 @@
 	trizen -S openssh --noconfirm
 	trizen -S vim --noconfirm
 	trizen -S rdesktop --noconfirm
-	trizen -S ardour --noconfirm
-	trizen -S gtop --noconfirm
-	trizen -S mps-youtube --noconfirm
-	trizen -S translate-shell --noconfirm #trans -T = language table / trans :en "[Enter] Meu coracao e forte!"
+	#trizen -S ardour --noconfirm
+	#trizen -S gtop --noconfirm
+	#trizen -S mps-youtube --noconfirm
+	#trizen -S translate-shell --noconfirm #trans -T = language table / trans :en "[Enter] Meu coracao e forte!"
 	trizen -S synapse --noconfirm 
 	trizen -S mtr --noconfirm
 	trizen -S nmap --noconfirm
 	trizen -S i3lock --noconfirm #set shortcut to: i3lock --color=000000
 	trizen -S plank --noconfirm
-	trizen -S evince --noconfirm
-	trizen -S caprine --noconfirm
-	trizen -S atom --noconfirm
-	trizen -S micro --noconfirm #terminal-based IDE
-	trizen -S neovim --noconfirm
-	trizen -S dynalist-desktop --noconfirm
-	trizen -S bmon --noconfirm
-	trizen -S mypaint --noconfirm
-	trizen -S viber --noconfirm
-	trizen -S typora --noconfirm
+	#trizen -S evince --noconfirm
+	#trizen -S caprine --noconfirm
+	#trizen -S atom --noconfirm
+	#trizen -S micro --noconfirm #terminal-based IDE
+	#trizen -S neovim --noconfirm
+	#trizen -S dynalist-desktop --noconfirm
+	#trizen -S bmon --noconfirm
+	#trizen -S mypaint --noconfirm
+	#trizen -S viber --noconfirm
+	#trizen -S typora --noconfirm
 	trizen -S okular --noconfirm #pdfviewer	
-	trizen -S snes9x-gtk #snes emulator
-	trizen -S oranchelo-icon-theme-git #oranchelo icon theme
+	#trizen -S snes9x-gtk #snes emulator
+	#trizen -S oranchelo-icon-theme-git #oranchelo icon theme
 	trizen -S viewnior --noconfirm #image viewer
 	trizen -S ag --noconfirm
 	trizen -S ncdu --noconfirm 
@@ -62,8 +62,8 @@
 	
 
 	#setting up Glances
-	trizen -S python-bottle --noconfirm #used for glances
-	trizen -S glances --noconfirm #awesome for monitoring
+	#trizen -S python-bottle --noconfirm #used for glances
+	#trizen -S glances --noconfirm #awesome for monitoring
 	#sudo glances -w (web server monitoring)
 	#sudo glances -w --username --password --port 2222 (example)
 	
@@ -81,9 +81,9 @@
 	systemctl start teamviewerd
 	
 	#setting up time (NTP)
-	trizen -S ntp --noconfirm
-	sudo timedatectl set-ntp true
-	sudo timedatectl set-timezone AmericaqFortaleza
+	#trizen -S ntp --noconfirm
+	#sudo timedatectl set-ntp true
+	#sudo timedatectl set-timezone AmericaqFortaleza
 	
 	#setting up ZSH
 	#trizen -S zsh --noconfirm
