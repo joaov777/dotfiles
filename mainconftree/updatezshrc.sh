419a95e72c5d9a628278462dbc9e01bf0953ce27
joaov777 <joaov777@gmail.com>
2019-04-15 15:47:25 -0300

    Melhorando o script de atualizacao do .zshrc

--- mainconftree/updatezshrc.sh
+++ mainconftree/updatezshrc.sh
@@ -4,8 +4,17 @@ clear
         echo '###### MAIN CONF ######'
         echo "- OPTION 3 - UPDATE .ZSHRC -"
         sleep 1
+	
+	zshrcPATH="~/.zshrc"
 
-            cat ~/mainconf/zsh/.zshrc > ~/.zshrc
-            sudo ln -sf ~/mainconf/zsh/.zshrc ~/.zshrc
-
-        clear
+	if [ -f $zshrcPATH ]
+	then	
+		echo ">>> File exists! Deleting!"
+		sudo rm ~/.zshrc
+	else
+		echo ">>> File non-existent! Creating!"
+		cat ~/mainconf/zsh/.zshrc > ~/.zshrc
+            	sudo ln -sf ~/mainconf/zsh/.zshrc ~/.zshrc
+	fi
+        
+	clear
