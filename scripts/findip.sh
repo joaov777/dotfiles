f7688103ab049c03681945b314c24f0e8bfa0e77
joaov777 <joaov777@gmail.com>
2019-06-11 14:52:20 -0300

    Improving findip again

--- scripts/findip.sh
+++ scripts/findip.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-        nmap -sP $1 >/dev/null && arp -an | grep $2 | awk '{print $2}' | sed 's/[()]//g'	
+        echo " -- IP FOUND: "$(nmap -sP $1 >/dev/null && arp -an | grep $2 | awk '{print $2}' | sed 's/[()]//g')
+		
