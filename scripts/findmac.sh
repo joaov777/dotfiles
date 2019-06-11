62c44b3cfa0007ced189b4784f273930c6a4ef6f
joaov777 <joaov777@gmail.com>
2019-06-11 14:39:40 -0300

    improving findmac.sh script

--- scripts/findmac.sh
+++ scripts/findmac.sh
@@ -4,6 +4,7 @@
 	read -p "IP: " ip
 	read -p "Interface: " i
 	
-	
-	echo ">> The MAC found is: "
-	sudo arping -I $i -c 1 $ip
+
+	res=$(sudo arping -I $i -c 1 $ip | grep 'reply from' | cut -d ' ' -f 5)	
+
+	echo ">> The MAC found is: $res"
