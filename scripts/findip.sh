a3fb32610174c731ca0e2717e31c336a67b4d0cf
joaov777 <joaov777@gmail.com>
2019-06-11 17:23:35 -0300

    Improving findip.sh

--- scripts/findip.sh
+++ scripts/findip.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-	echo ">>> FIND IP ADDRESS <<<"
-	read -p ">> Network/Mask:" n
-	read -p ">> MAC:" m
-
-	echo "IP found: " 
-	nmap -sP $n >/dev/null && arp -an | grep $m | awk '{print $2}' | sed 's/[()]//g'
+        nmap -sP $1 >/dev/null && arp -an | grep $2 | awk '{print $2}' | sed 's/[()]//g'	
