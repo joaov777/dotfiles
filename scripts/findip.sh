1cc84a08094eb66d938d9907e7c18e15e421eb52
joaov777 <joaov777@gmail.com>
2019-06-12 07:56:04 -0300

    Improving scripts

--- scripts/findip.sh
+++ scripts/findip.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-        echo " -- IP FOUND: "$(nmap -sP $1 >/dev/null && arp -an | grep $2 | awk '{print $2}' | sed 's/[()]//g')
-		
+        
+	#previous commmand used
+	#echo " -- IP FOUND: "$(nmap -sP $1 >/dev/null && arp -an | grep $2 | awk '{print $2}' | sed 's/[()]//g')
+	
+	#usage findip 192.168.0.0/24 85:78:r5
+        echo " -- IP FOUND: "$(sudo arp-scan $1 | grep $2 | cut -f 1)
+
--- scripts/findmac.sh
+++ scripts/findmac.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-	echo " -- MAC FOUND: "$(sudo arping -I $1 -c 1 $2 | grep 'reply from' | cut -d ' ' -f 5)	
+	#previous command
+	#echo " -- MAC FOUND: "$(sudo arping -I $1 -c 1 $2 | grep 'reply from' | cut -d ' ' -f 5)	
 
+	#echo " -- MAC FOUND: "$()echo " -- IP FOUND: "$(sudo arp-scan $1 | grep $2 | cut -f 2)
+
+
+	#Correct command
+	echo "-- MAC FOUND: "$(sudo arp-scan $1 | sed -n '3p' | cut -f 2,3)
+	 
