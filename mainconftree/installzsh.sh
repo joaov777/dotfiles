406506d27e618c96dde9001d2fe0179ad41f3428
joaov777 <joaov777@gmail.com>
2019-07-14 10:16:48 -0300

    Adding link

--- mainconftree/installzsh.sh
+++ mainconftree/installzsh.sh
@@ -19,7 +19,7 @@ clear
 	    cd ~/.oh-my-zsh/custom/plugins
 	    git clone https://github.com/zsh-users/zsh-autosuggestions
 		
-		#installing pi theme
+		#installing pi theme https://github.com/tobyjamesthomas/pi
 		echo ">> Installing zsh pi theme"
 	    cd ~/.oh-my-zsh/custom/themes
 	    git clone https://github.com/tobyjamesthomas/pi.git
