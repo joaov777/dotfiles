a3d4a9ddad1a4280d349b8f4fa22d5c91a269d21
joaov777 <joaov777@gmail.com>
2019-07-15 15:34:35 -0300

    Improving git user setup logic

--- mainconftree/setupgituser.sh
+++ mainconftree/setupgituser.sh
@@ -1,16 +1,62 @@
 #!/bin/bash
 
+install_git() {
+	sudo pacman -S git --noconfirm
+}
+
+check_git() {
+	pacman -Q | grep git
+}
+
+user_setup() {
+	echo '>> User setup...'
+	read -p "Git username: " gitusername
+	read -p "Git email:" gitemail
+
+    	sudo git config --global user.name "$gitusername"
+        sudo git config --global user.email "$gitemail"
+        sudo git config --global core.editor vim
+}
+
 clear
         echo '###### MAIN CONF ######'
         echo "- OPTION 1 - SETUP GIT USER -"
         sleep 1
-        
-        echo '>User setup...'
-        read -p "Git username: " gitusername
-        read -p "Git email:" gitemail
-        
-            	sudo git config --global user.name "$gitusername"
-	        sudo git config --global user.email "$gitemail"
-	        sudo git config --global core.editor vim
+
+	res=$(pacman -Q | grep git)
+
+	if [ -z "$res" ];
+	then
+		echo ">> Git is not installed!!"
+		echo ">> Installing git!!"
+		install_git
+
+		echo ">> Git installed successfuly!!"
+
+		user_setup
+
+		echo '>> User properly assigned!!'
+		sleep 1
+
+		clear
+	else
+		echo ">> Git is already installed!!"
+		user_setup
+	fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         clear
