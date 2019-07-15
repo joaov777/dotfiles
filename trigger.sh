d268516aa5d49a6be49c11addaf96aee773e9f0c
joaov777 <joaov777@gmail.com>
2019-07-15 15:05:54 -0300

    Improving trigger logic

--- trigger.sh
+++ trigger.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+triggercore() {
 	cd ~
 	wget -q https://github.com/joaov777/mainconf/archive/master.zip
 	sudo unzip -qq master.zip
@@ -8,3 +9,18 @@
 	sudo chown -R joao:joao mainconf/
 	sudo rm master.zip
 	rm -- "$0" 
+}
+
+
+	if [ ! -d ~/mainconf ]; 
+	then
+		triggercore
+	else
+		echo ">> Mainconf folder exists!!" sleep 1
+		echo ">> Deleting it!!" sleep 1
+		sudo rm -rf ~/mainconf/ >> /dev/null
+		
+		echo ">> Mainconf folder deleted!!" sleep 1
+		echo ">> Restarting script!!" sleep 1
+		triggercore
+	fi
