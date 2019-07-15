096c19afd83a590f7e9f2606c9f3bcfd174d793b
joaov777 <joaov777@gmail.com>
2019-07-15 17:43:17 -0300

    Major update on updatepackages

--- updatePackages/updatePackages.sh
+++ updatePackages/updatePackages.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-	sudo pacman -S trizen --noconfirm
+	#installing trizen helper
+	git clone https://aur.archlinux.org/trizen-git.git >> /dev/null ; \
+	cd trizen-git ; \
+	makepkg -si >> /dev/null ; \
+	sudo rm -rf trizen-git
+
+	#sudo pacman -S trizen --noconfirm
 	trizen -S git --noconfirm
-	trizen -S mellowplayer --noconfirm
 	trizen -S etcher --noconfirm
 	trizen -S notepadqq --noconfirm
 	trizen -S gparted --noconfirm
 	trizen -S keeweb-desktop --noconfirm
 	trizen -S filezilla --noconfirm
 	trizen -S telegram-desktop --noconfirm	
-	trizen -S google-chrome --noconfirm
-	trizen -S ranger --noconfirm
 	trizen -S clipit --noconfirm
 	trizen -S flameshot --noconfirm
 	trizen -S guake --noconfirm
-	trizen -S remmina --noconfirm
-	trizen -S simplenote-electron-bin --noconfirm
 	trizen -S gedit --noconfirm
-	trizen -S barrier --noconfirm	
-	trizen -S anydesk --noconfirm
 	trizen -S pwgen --noconfirm
 	trizen -S openssh --noconfirm
 	trizen -S vim --noconfirm
 	trizen -S rdesktop --noconfirm
-	trizen -S scrot --noconfirm
 	trizen -S i3lock --noconfirm #set shortcut to: i3lock --color=000000
 	trizen -S synapse --noconfirm 
 	trizen -S mtr --noconfirm
@@ -33,19 +31,17 @@
 	trizen -S ncdu --noconfirm 
 	trizen -S inxi --noconfirm
 	trizen -S otf-fira-mono --noconfirm
-	trizen -S visual-studio-code-bin --noconfirm
 	trizen -S pdfarranger-git --noconfirm #awesome tool for pdf arrangement
 	trizen -S ipcalc --noconfirm
 	trizen -S sipcalc --noconfirm #sipcalc -a 10.10.10.10/27
 	trizen -S whatmask --noconfirm #ex.: whatmask /27 # whatmask 10.10.10.10/28 
 	trizen -S shc --noconfirm #useful for encrypting scripts Ex.: shc -f <file_path>
 	trizen -S veracrypt --noconfirm
-	trizen -S skypeforlinux-preview-bin --noconfirm
 	trizen -S obs-studio --noconfirm
-	trizen -S snes9x-gtk --noconfirm #snes emulator
-	trizen -S vinagre --noconfirm #rdp clientvina
 	trizen -S arp-scan --noconfirm
 	trizen -S net-tools --noconfirm
+	trizen -S visual-studio-code-bin --noconfirm
+	trizen -S google-chrome --noconfirm
 
 
 	#Downloading papirus icon themes and setting up to grey
@@ -57,8 +53,10 @@
 	#Setting up Teamviewer
 	trizen -S teamviewer --noconfirm
 	sudo teamviewer --daemon enable
-	systemctl enable teamviewerd
-	systemctl start teamviewerd
+	sudo systemctl enable teamviewerd.service
+	sudo systemctl start teamveiwerd.service
+	
+
 
 	#setting up Glances
 	#trizen -S python-bottle --noconfirm #used for glances
@@ -77,6 +75,14 @@
 	#sudo timedatectl set-ntp true
 	#sudo timedatectl set-timezone America/Fortaleza
 
+	#trizen -S skypeforlinux-preview-bin --noconfirm
+	#trizen -S snes9x-gtk --noconfirm #snes emulator
+	#trizen -S scrot --noconfirm
+	#trizen -S barrier --noconfirm	
+	#trizen -S anydesk --noconfirm
+	#trizen -S remmina --noconfirm
+	#trizen -S simplenote-electron-bin --noconfirm
+	#trizen -S ranger --noconfirm
 	#trizen -S micro --noconfirm #terminal-based IDE
 	#trizen -S abntex2 --noconfirm #for latex
 	#trizen -S i3lock-fancy-git --noconfirm #i3lock-fancy -- scrot -z
@@ -93,5 +99,6 @@
 	#trizen -S typora --noconfirm
 	#trizen -S oranchelo-icon-theme-git #oranchelo icon theme
 	#trizen -S translate-shell --noconfirm #trans -T = language table / trans :en "[Enter] Meu coracao e forte!"
+	#trizen -S mellowplayer --noconfirm
 
 	
