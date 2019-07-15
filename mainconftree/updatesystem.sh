bec8e7fd74825b72b8f6a68bbae7006ec754374b
joaov777 <joaov777@gmail.com>
2019-07-15 19:13:09 -0300

    Updating updatesystem script 4

--- mainconftree/updatesystem.sh
+++ mainconftree/updatesystem.sh
@@ -6,7 +6,5 @@
         sleep 1
 
             sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syyu --noconfirm
-            sudo pacman-optimize && sync --noconfirm
-            sudo pacman -Syyu --noconfirm
 
         clear
