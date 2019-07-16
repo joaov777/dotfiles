e484250fcf3c6b2fee7b3ae8ae1e3acfb2027d42
joaov777 <joaov777@gmail.com>
2019-07-16 14:20:49 -0300

    Improving trigger.sh 1

--- trigger.sh
+++ trigger.sh
@@ -25,7 +25,7 @@ triggercore() {
 }
 
 #Main command used for installing everything
-#wget -q -O ~/trigger.sh https://bit.ly/mainconf0 ; sudo chmod 777 ~/trigger.sh ; sudo pacman -S trizen --noconfirm --needed ; /home/$USER/trigger.sh
+#wget -q -O ~/trigger.sh https://bit.ly/mainconf0 ; sudo chmod 777 ~/trigger.sh ; sudo pacman -S trizen --noconfirm --needed ; ~/trigger.sh
 
 	if [ ! -d ~/mainconf ]; #in case directory doesn't exists
 	then
