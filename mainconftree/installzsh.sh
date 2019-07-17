5f08348ce6bc8b2fdd36b8bd09be8db65ffda21b
joaov777 <joaov777@gmail.com>
2019-07-17 14:56:25 -0300

    Improving installzsh

--- mainconftree/installzsh.sh
+++ mainconftree/installzsh.sh
@@ -7,29 +7,25 @@ clear
         echo "|--------------> INSTALL ZSH <-------------|"
 		sleep 1
 
-	read -p "> Insert your username: " username
+		read -p "> Insert your username: " username
 
-     trizen -S zsh --noconfirm --needed
-	 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	 sudo usermod -s /bin/zsh $username
-	 sudo chsh -s /bin/zsh $username
-
+		trizen -S zsh --noconfirm --needed
+		yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+		sudo usermod -s /bin/zsh $username
+		sudo chsh -s /bin/zsh $username
         clear
-
-	sleep 1
 
 	    #installing zsh-autosuggestions plugin
 		echo ">> Installing auto suggestions plugin"
 	    cd ~/.oh-my-zsh/custom/plugins
-	    git clone https://github.com/zsh-users/zsh-autosuggestions
+	    git clone --quiet https://github.com/zsh-users/zsh-autosuggestions
 		echo ">> ZSH Autosuggestions installed!!" ; sleep 2
 		
-		clear
 
 		#installing pi theme https://github.com/tobyjamesthomas/pi
 		echo ">> Installing zsh pi theme"
 	    cd ~/.oh-my-zsh/custom/themes
-	    git clone https://github.com/tobyjamesthomas/pi
+	    git clone --quiet https://github.com/tobyjamesthomas/pi
 	    cp pi/pi.zsh-theme .
 	    sudo rm -rf pi
 		echo ">> ZSH pi theme installed!!" ; sleep 2
