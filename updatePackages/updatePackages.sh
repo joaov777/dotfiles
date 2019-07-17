b3af147d058f7f86ac01d797b3bcc25771638831
joaov777 <joaov777@gmail.com>
2019-07-17 10:46:20 -0300

    Testing - major update on updatepackages

--- updatePackages/updatePackages.sh
+++ updatePackages/updatePackages.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
+
+#list of packages
+#packages_required=(
+#	git tilda etcher notepadqq gparted keeweb-desktop \ 
+#	filezilla telegram-desktop clipit flameshot guake \
+#	gedit pwgen openssh vim rdesktop i3lock synapse mtr \
+#	nmap okular viewnior ncdu inxi otf-fira-mono \
+#	pdfarranger-git ipcalc sipcalc whatmask veracrypt \
+#	obs-studio arp-scan net-tools visual-studio-code-bin \
+#	google-chrome teamviewer keyboardctl
+#
+#	papirus-icon-theme papirus-folders-git			
+#					)
+#
+#for pkg in "${packages_required[@]}"; do
+#	if [ -z "$(pacman -Qi $pkg)" ]; then #in case it is not installed
+#		echo ">> $pkg is not installed!!" ; sleep 1
+#		echo ">> Installing $pkg..." ; sleep 1
+#		trizen -S "$pkg" --noconfirm --needed >> /dev/null
+#		
+#			#checking whether a package has successfuly been installed
+#			if [ -z "$(pacman -Qi $pkg)" ]; then
+#				echo ">> ERROR: Package not installed" ; sleep 2
+#			else
+#				echo ">> SUCCESS: Package $pkg installed!!" ; sleep 2
+#			fi
+#	else
+#		echo ">> $pkg is installed!!" ; sleep 1 #in case it is installed already
+#	fi
+#done
+#
+#	sudo teamviewer --daemon enable
+#	sudo systemctl enable teamviewerd.service
+#	sudo systemctl start teamveiwerd.service
+#
+#	sudo papirus-folders -C grey 
+
+#################################
+#################################
 	
 
 	# MAIN PACKAGES 
