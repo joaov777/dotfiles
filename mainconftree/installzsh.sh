460508863ca754d4fe4706ddaeffe885efccc98a
joaov777 <joaov777@gmail.com>
2019-07-17 14:58:09 -0300

    Improving installzsh - 001

--- mainconftree/installzsh.sh
+++ mainconftree/installzsh.sh
@@ -13,15 +13,13 @@ clear
 		yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 		sudo usermod -s /bin/zsh $username
 		sudo chsh -s /bin/zsh $username
-        clear
 
 	    #installing zsh-autosuggestions plugin
 		echo ">> Installing auto suggestions plugin"
 	    cd ~/.oh-my-zsh/custom/plugins
 	    git clone --quiet https://github.com/zsh-users/zsh-autosuggestions
 		echo ">> ZSH Autosuggestions installed!!" ; sleep 2
-		
-
+	
 		#installing pi theme https://github.com/tobyjamesthomas/pi
 		echo ">> Installing zsh pi theme"
 	    cd ~/.oh-my-zsh/custom/themes
