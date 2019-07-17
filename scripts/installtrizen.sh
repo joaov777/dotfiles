a85cdc148a6b2cf72e409630420f4395c029f830
joaov777 <joaov777@gmail.com>
2019-07-17 08:47:05 -0300

    Testing on trizen installation 4

--- scripts/installtrizen.sh
+++ scripts/installtrizen.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-    cd ~ 
-    git clone https://aur.archlinux.org/trizen-git.git
-    cd trizen-git
-    yes | makepkg -si
+install_trizen() {
+        cd ~ 
+        git clone https://aur.archlinux.org/trizen-git.git
+        cd trizen-git
+        yes | makepkg -si
 
-    cd ..
-    rm -rf trizen-git
+        cd ..
+        rm -rf trizen-git
+
+}
+
+
+    if [ -z "$(pacman -Qi trizen)" ];
+	then
+		echo ">> Trizen is not installed!!" ; sleep 2
+        echo ">> Installing Trizen AUR Helper" ; sleep 3
+		install_trizen    
+	else
+		echo ">> Git is already installed!!" ; sleep 2
+	fi
+
+    
+    
+
+
+    
     
