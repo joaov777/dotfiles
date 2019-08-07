74fad4af2362a6978cf533c8a7e9fb9f77b21fe2
joaov777 <joaov777@gmail.com>
2019-08-07 08:32:06 -0300

    Adding bkpolo script improvement 3

--- scripts/bkpolo.sh
+++ scripts/bkpolo.sh
@@ -2,24 +2,19 @@
 
 main_procedure(){
 
-       file="config 00$n - $(date +%d-%m-%y) - $(date +%T).xml"
+    n=$(rclone ls coir:/Backups/Firewalls/Polo\ de\ Inovacao/Global | wc -l)
+    if ! [ -d ~/bkp ]; then mkdir ~/bkp ; fi
 
-        scp -P 5555 joao.galvino@200.129.18.2:/cf/conf/config.xml ~/bkp/"$file"
-        echo "config.xml downloaded!!!"
-        rclone copy ~/bkp/"$file" coir:/Backups/Firewalls/Polo\ de\ Inovacao/Global
-        echo "config.xml uploaded to Google Drive!!!"
+    file="config 00$n - $(date +%d-%m-%y) - $(date +%T).xml"
+
+    scp -P 5555 joao.galvino@200.129.18.2:/cf/conf/config.xml ~/bkp/"$file" > /dev/null
+    echo "|> config.xml downloaded!!!"
+    rclone copy ~/bkp/"$file" coir:/Backups/Firewalls/Polo\ de\ Inovacao/Global
+    echo "|> config.xml uploaded to Google Drive!!!" ;
+    rm -rf ~/bkp
 
 }
-   
-    n=$(rclone ls coir:/Backups/Firewalls/Polo\ de\ Inovacao/Global | wc -l)
-    if [ -d ~/bkp ]
-    then
-        main_procedure
-    else
-        mkdir ~/bkp
-        main_procedure
 
-        #scp -P 5555 joao.galvino@200.129.18.2:/cf/conf/config.xml ~/Documents/firewalls/polo/config$(ls -1 | wc -l).xml
-    fi
-    rm -rf ~/bkp
+    echo "|>> Pfsense - config.xml backup <<|"
+    main_procedure
     
