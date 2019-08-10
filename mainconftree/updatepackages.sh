e0f96abee8a48fd7d1c67aa129a007217f18f943
joaov777 <joaov777@gmail.com>
2019-08-10 08:22:48 -0300

    removing synapse from update packages

--- mainconftree/updatepackages.sh
+++ mainconftree/updatepackages.sh
@@ -30,7 +30,7 @@ packages_required=(
 	whatmask visual-studio-code-bin google-chrome \
 	git tilda etcher notepadqq gparted papirus-folders-git \
 	filezilla telegram-desktop clipit flameshot guake \
-	gedit pwgen openssh vim rdesktop i3lock synapse mtr tmux \
+	gedit pwgen openssh vim rdesktop i3lock mtr tmux \
 	nmap okular viewnior ncdu inxi otf-fira-mono nordvpn-bin \
 	ipcalc sipcalc whatmask veracrypt papirus-icon-theme \
 	obs-studio arp-scan net-tools teamviewer keyboardctl rclone \ 
