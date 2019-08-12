91b4f0e5db71cb1617ae2497e436ef90059a8a73
joaov777 <joaov777@gmail.com>
2019-08-12 10:16:42 -0300

    Final solution for bkpolo4 script - 001

--- varied/wiki/bkpolo4.sh
+++ varied/wiki/bkpolo4.sh
@@ -1,47 +1,62 @@
 #!/bin/bash
 
 # REQUIREMENTS FOR BETTER USAGE
-# - Local account SSH keys properly set up in the target firewall (ssh-copy-id)
-# - rclone target drive properly set up       
+# - Usuário "backup" deverá ser criado no firewall remoto com autenticaão por chaves assimétricas.
+# - O local de execução deste script deverá possuir a chave pública mapeada nos firewalls remotos (conta "backup")
+# - Rclone deverá ser corretamente mapeado no mesmo local de instalação/execução deste script 
 
+# declaração da associação dos campus
 declare -A campus_keys
 campus_keys['200.129.18.2']='Polo de Inovacao'
+campus_keys['192.168.19.2']='Local de teste'
 campus_keys['200.129.16.1']='Reitoria'
 
+# lista de campus 
 campus_list=(
 
+    192.168.19.2 \
     200.129.18.2 \
     200.129.16.1 \
 
 )
 
+# função principal
 main_procedure(){
     
 for pkg in "${campus_list[@]}"; do
 	clear
 	echo "|==============|> PFSENSE XML BACKUP <|==============|"
-    echo "|=====> Campus ${campus_keys["$pkg"]}"
-    if ! [ -d ~/bkp ]; then mkdir ~/bkp ; fi
+    echo "|=====> Campus: ${campus_keys["$pkg"]}"
+    if [ ! -d ~/bkp ]; then mkdir ~/bkp ; fi #checando se existe a pasta temporaria local
 
-    mkdir ~/bkp/"${campus_keys["$pkg"]}"
+    # checando se existe a pasta do campus especifico em bkp
+    if [ ! -d ~/bkp/"${campus_keys["$pkg"]}" ]; then mkdir ~/bkp/"${campus_keys["$pkg"]}" ; fi
 
-    #checking campus remote folder existence
-    if [[ $(rclone tree coir:/Backups/Firewalls | grep ${campus_keys["$pkg"]}) = "" ]]; 
-        then rclone mkdir coir:/Backups/Firewalls/"${campus_keys["$pkg"]}"/Global
+    # checando a existencia da pasta do campus no google drive
+    if [[ $(rclone tree coir:/Backups/Firewalls | grep "${campus_keys["$pkg"]}") = "" ]];
+        then rclone mkdir coir:/Backups/Firewalls/"${campus_keys["$pkg"]}"
     fi
 
-    n=$(rclone ls coir:/Backups/Firewalls/"${campus_keys["$pkg"]}"/Global/ | wc -l)
-    file="config - ${campus_keys["$pkg"]} - 00$n - $(date +%d-%m-%y) - $(date +%T).xml"
+    # retornando a quantidade de backups feitos pelo campus especifico
+    n=$(rclone ls coir:/Backups/Firewalls/"${campus_keys["$pkg"]}" | wc -l)
 
-    scp backup@$pkg:/cf/conf/config.xml ~/bkp/"${campus_keys["$pkg"]}"/"$file" > /dev/null
+    # configurando a nomenclatura utilizada para o arquivo .xml
+    file="config - 00$n - ${campus_keys["$pkg"]} - $(date +%d-%m-%y) - $(date +%T).xml"
+
+    # download do arquivo .xml
+    scp -P 5555 backup@$pkg:/cf/conf/config.xml ~/bkp/"${campus_keys["$pkg"]}"/"$file" > /dev/null
     echo "|===> config.xml downloaded!!!"
-    rclone copy ~/bkp/"${campus_keys["$pkg"]}"/"$file" coir:/Backups/Firewalls/"${campus_keys["$pkg"]}"/Global
+
+    # envio do arquivo .xml ao google drive
+    rclone copy ~/bkp/"${campus_keys["$pkg"]}"/"$file" coir:/Backups/Firewalls/"${campus_keys["$pkg"]}"
     echo "|===> config.xml uploaded to Google Drive!!!" ; sleep 2;
     
+    done 
 
-    done
+    # remocao de lixo da maquina local
     rm -rf ~/bkp
 }
+    # execucao da função principal
     main_procedure
     
     
