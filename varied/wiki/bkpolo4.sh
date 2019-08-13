bb0ca101eedbdc22531c75b779280d1b5df57ee4
joaov777 <joaov777@gmail.com>
2019-08-13 12:44:23 -0300

    Final solution for bkpolo4 script - 004

--- varied/wiki/bkpolo4.sh
+++ varied/wiki/bkpolo4.sh
@@ -10,49 +10,44 @@ declare -A campus_keys
 campus_keys['200.129.18.2']='Polo de Inovacao'
 campus_keys['200.129.16.1']='Reitoria'
 
-# lista de campus 
-campus_list=(
-
-    200.129.18.2 \
-    200.129.16.1 \
-
-)
-
 # função principal
 main_procedure(){
     
-for pkg in "${campus_list[@]}"; do
+for pkg in "${!campus_keys[@]}"; do
 	clear
 	echo "|==============|> PFSENSE XML BACKUP <|==============|"
-    echo "|=====> Campus: ${campus_keys["$pkg"]}"
+    echo "|=====> Campus: ${campus_keys[$pkg]}"
+
     if [ ! -d ~/bkp ]; then mkdir ~/bkp ; fi #checando se existe a pasta temporaria local
 
     # checando se existe a pasta do campus especifico em bkp
-    if [ ! -d ~/bkp/"${campus_keys["$pkg"]}" ]; then mkdir ~/bkp/"${campus_keys["$pkg"]}" ; fi
+    if [ ! -d ~/bkp/"${campus_keys[$pkg]}" ]; then mkdir ~/bkp/"${campus_keys[$pkg]}" ; fi
 
     # checando a existencia da pasta do campus no google drive
-    if [[ $(rclone tree coir:/Backups/Firewalls | grep "${campus_keys["$pkg"]}") = "" ]];
-        then rclone mkdir coir:/Backups/Firewalls/"${campus_keys["$pkg"]}"
+    if [[ $(rclone tree coir:/Backups/Firewalls | grep "${campus_keys[$pkg]}") = "" ]];
+        then rclone mkdir coir:/Backups/Firewalls/"${campus_keys[$pkg]}"
     fi
 
     # retornando a quantidade de backups feitos pelo campus especifico
-    n=$(rclone ls coir:/Backups/Firewalls/"${campus_keys["$pkg"]}" | wc -l)
+    n=$(rclone ls coir:/Backups/Firewalls/"${campus_keys[$pkg]}" | wc -l)
 
     # configurando a nomenclatura utilizada para o arquivo .xml
-    file="config - 00$n - ${campus_keys["$pkg"]} - $(date +%d-%m-%y) - $(date +%T).xml"
+    file="config - 00$n - ${campus_keys[$pkg]} - $(date +%d-%m-%y) - $(date +%T).xml"
 
     # download do arquivo .xml
-    scp -P 5555 backup@$pkg:/cf/conf/config.xml ~/bkp/"${campus_keys["$pkg"]}"/"$file" > /dev/null
+    scp -P 22 backup@$pkg:/cf/conf/config.xml ~/bkp/"${campus_keys["$pkg"]}"/"$file" > /dev/null
     echo "|===> config.xml downloaded!!!"
 
     # envio do arquivo .xml ao google drive
-    rclone copy ~/bkp/"${campus_keys["$pkg"]}"/"$file" coir:/Backups/Firewalls/"${campus_keys["$pkg"]}"
+    rclone copy ~/bkp/"${campus_keys[$pkg]}"/"$file" coir:/Backups/Firewalls/"${campus_keys[$pkg]}"
     echo "|===> config.xml uploaded to Google Drive!!!" ; sleep 2;
     
     done 
 
     # remocao de lixo da maquina local
     rm -rf ~/bkp
+
+
 }
     # execucao da função principal
     main_procedure
