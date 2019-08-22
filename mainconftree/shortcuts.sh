174f7da1fcf3952ee531fe39f5ff41d1dd5263ca
joaov777 <joaov777@gmail.com>
2019-08-22 20:27:12 -0300

    Adding code improvement for shortcuts

--- mainconftree/shortcuts.sh
+++ mainconftree/shortcuts.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
+check_file() {
+    if [ -f "$1" ]; 
+    then    
+        echo "----> Working on files..." ; sleep 1
+        rm "$1"
+    fi
+
+    }
+
     clear
 		echo "$(tput bold)$(tput setaf 7)"
         echo "|==============|> MAINCONF <|==============|"
         echo "|------------> ENABLE SHORTCUTS AND PANEL <-----------|"
 
     echo "|----> Copying shortcuts..." ; sleep 1
-    #curl https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-keyboard-shortcuts.xml > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+    check_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+    check_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+
+    #working on shortcuts
     wget -q -O ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-keyboard-shortcuts.xml
+    ln -sf ~/mainconf/varied/wiki/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+
+    #working on xsettings
     wget -q -O ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xsettings.xml
+    ln -sf ~/mainconf/varied/wiki/xsettings.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
     
     echo "|----> Updating shortcuts..." ; sleep 1
     kill -9 $(pidof xfconfd)
@@ -19,7 +35,6 @@
 
 
     echo "|----> Copying xfce panel..." ; sleep 1
-    #curl https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-panel.xml > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
     rm ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml > /dev/null
     wget -q -O ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-panel.xml
 
@@ -27,6 +42,7 @@
     xfce4-panel -r > /dev/null
 
     echo "|----> Panel updated..." ; sleep 2
+
     
     
 
