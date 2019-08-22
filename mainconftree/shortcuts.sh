aa5049cb9620ade210719780af6b5e9cb0dcf026
joaov777 <joaov777@gmail.com>
2019-08-22 20:45:50 -0300

    Adding code improvement for shortcuts - 001

--- mainconftree/shortcuts.sh
+++ mainconftree/shortcuts.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-check_file() {
-    if [ -f "$1" ]; 
-    then    
-        echo "----> Working on files..." ; sleep 1
-        rm "$1"
-    fi
 
-    }
 
     clear
 		echo "$(tput bold)$(tput setaf 7)"
         echo "|==============|> MAINCONF <|==============|"
         echo "|------------> ENABLE SHORTCUTS AND PANEL <-----------|"
 
-    echo "|----> Copying shortcuts..." ; sleep 1
-    check_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-    check_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+    echo "|----> Copying shortcuts and xsettings..." ; sleep 1
+
+    #variables
+    shortcuts=~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+    xsettings=~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+    panel=~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+
+    #deleting previous configs
+    if [ -f $shortcuts ]; then echo "----> Deleting shortcuts.xml" ; rm $shortcuts > /dev/null ; fi
+    if [ -f $xsettings ]; then echo "----> Deleting xsettings.xml" ; rm $xsettings > /dev/null ; fi
 
     #working on shortcuts
-    wget -q -O ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-keyboard-shortcuts.xml
-    ln -sf ~/mainconf/varied/wiki/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+    wget -q -O $shortcuts https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-keyboard-shortcuts.xml
+    ln -sf ~/mainconf/varied/wiki/xfce4-keyboard-shortcuts.xml $shortcuts
 
     #working on xsettings
-    wget -q -O ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xsettings.xml
-    ln -sf ~/mainconf/varied/wiki/xsettings.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+    wget -q -O $xsettings https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xsettings.xml
+    ln -sf ~/mainconf/varied/wiki/xsettings.xml $xsettings
     
     echo "|----> Updating shortcuts..." ; sleep 1
     kill -9 $(pidof xfconfd)
@@ -35,8 +35,9 @@ check_file() {
 
 
     echo "|----> Copying xfce panel..." ; sleep 1
-    rm ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml > /dev/null
-    wget -q -O ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-panel.xml
+    if [ -f $panel ]; then echo "----> Deleting xfce4-panel.xml" ; rm $panel > /dev/null ; fi
+
+    wget -q -O $panel https://raw.githubusercontent.com/joaov777/mainconf/master/varied/wiki/xfce4-panel.xml
 
     echo "|----> Updating panel..." ; sleep 2
     xfce4-panel -r > /dev/null
