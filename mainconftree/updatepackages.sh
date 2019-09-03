b06d88d2f9881b7ee4e8ba3305aa490ada2c6dea
joaov777 <joaov777@gmail.com>
2019-09-03 14:46:20 -0300

    Improving update packages 001

--- mainconftree/updatepackages.sh
+++ mainconftree/updatepackages.sh
@@ -47,6 +47,8 @@ check_installation() {
 	#installing trizen
 	install_trizen
 
+	read -s -p "$USER password: " $userpass
+
 for pkg in "${packages_required[@]}"; do
 	clear
 	echo "$(tput bold)$(tput setaf 7)"
@@ -56,7 +58,7 @@ for pkg in "${packages_required[@]}"; do
 	if [ -z "$(pacman -Qi $pkg)" ]; then #in case it is not installed
 		echo ">> $pkg is not installed!!" ; sleep 1
 		echo ">> Installing $pkg..." ; sleep 1
-		yes | trizen -S "$pkg" --noconfirm --needed &>/dev/null
+			$userpass | trizen -S "$pkg" --noconfirm --needed &>/dev/null
 	
 			#checking whether a package has successfuly been installed -
 			check_installation "$pkg"
