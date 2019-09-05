85db32d0bff1c4f0b33f73a7ddc2ed2c77d727d8
joaov777 <joaov777@gmail.com>
2019-09-05 10:57:53 -0300

    Adding powerline fonts to install zsh for pi theme 003

--- mainconftree/installzsh.sh
+++ mainconftree/installzsh.sh
@@ -15,7 +15,7 @@ clear
   			sudo pacman -S zsh --noconfirm --needed
 		fi
 
-		echo "|---> Checking $HOME/.oh-my-zsh folder" ; sleep 1
+		echo "|---> Checking $HOME/.oh-my-zsh folder" ; sleep 2
 		if [ -d ~/.oh-my-zsh ]; then sudo rm -rf ~/.oh-my-zsh ; fi
 
 		yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -23,26 +23,26 @@ clear
 		sudo chsh -s /bin/zsh $username
 
 	    #installing zsh-autosuggestions plugin
-		echo "|---> Installing auto suggestions plugin"
+		echo "|---> Installing auto suggestions plugin" ; sleep 2
 		if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
 		sudo rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 		fi
 		git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 		echo "|---> ZSH Autosuggestions installed!!" ; sleep 2
 		
-		echo "|---> Downloading Powerline fonts"
+		#installing powerline fonts
+		echo "|---> Downloading Powerline fonts" ; sleep 2
 		git clone --quiet https://github.com/powerline/fonts.git $HOME/.local/share/fonts/
 		$HOME/.local/share/fonts/install.sh
 		echo "|---> Powerline fonts installed!!" ; sleep 2
 	
 		#installing pi theme https://github.com/tobyjamesthomas/pi
-		echo "|---> Installing zsh pi theme"
+		echo "|---> Installing zsh pi theme" ; sleep 2
 	    if [ -d ~/.oh-my-zsh/custom/themes/pi ]; then
 		sudo rm -rf ~/.oh-my-zsh/custom/themes/pi
 		fi
 	    
 		wget -O /home/joao/.oh-my-zsh/custom/themes/pi.zsh-theme https://raw.githubusercontent.com/joaov777/mainconf/master/zsh/pi.zsh-theme
-
 		echo "|---> ZSH pi theme installed!!" ; sleep 2
 
 		
