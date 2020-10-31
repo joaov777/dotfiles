97fa8126c930efb646bcbfaad8c9c900cbe419e7
joaov777 <joaov777@gmail.com>
2020-10-31 12:24:29 -0300

    Adding a few improvements

--- scripts/usdtobrl.sh
+++ scripts/usdtobrl.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 	
-	dollar_rate=$(curl -s https://www.x-rates.com/table/\?from\=USD\&amount\=1 | grep ";to=BRL" | awk '{print $3}' | cut -c58-61) &> /dev/null 2>&1
+	dollar_rate=$(curl -s https://www.x-rates.com/table/\?from\=USD\&amount\=1 | grep ";to=BRL" | awk '{print $3}' | cut -c59-62) &> /dev/null 2>&1
 	echo "> Current dollar rate: $dollar_rate"
 	
 	while [ true ];
