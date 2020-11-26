5f6623f46d67ffd76fa98d588050528ee01935be
joaov777 <joaov777@gmail.com>
2020-11-26 16:57:17 -0300

    Adding third tilix style

--- mainconftree/enableconfigfiles.sh
+++ mainconftree/enableconfigfiles.sh
@@ -33,7 +33,7 @@ clear
         sudo ln -sf ~/mainconf/varied/i3related/py3status/py3statusbars/i3status2.conf /etc/i3status2.conf
 
         echo "Enabling Tilix" ; sleep 1
-        dconf load /com/gexperts/Tilix/ < ~/mainconf/varied/tilix/tilix2.dconf
+        dconf load /com/gexperts/Tilix/ < ~/mainconf/varied/tilix/tilix3.dconf
 
         echo "Enabling rofi launcher..." ; sleep 1
         [ ! -d "~/.config/rofi" ] && mkdir ~/.config/rofi || echo "Folder exists!"
