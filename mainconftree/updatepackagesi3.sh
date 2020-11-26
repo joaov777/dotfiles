a3625f33d2a29f8b2a63e7a2501a8d29e71cb59a
joaov777 <joaov777@gmail.com>
2020-11-26 16:51:02 -0300

    Adding Tilix to i3 updatepackates

--- mainconftree/updatepackagesi3.sh
+++ mainconftree/updatepackagesi3.sh
@@ -35,7 +35,7 @@ packages_required=(
 	veracrypt papirus-icon-theme neofetch lxappearance vlc picom i3status trayer \
 	arp-scan net-tools teamviewer rclone peek xorg-server xorg-apps xorg-init networkmanager-dmenu-git \ 
 	imagewriter albert wget dnsutils xorg-xrandr arandr sshfs nm-connection-editor \ 
-	pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils blueman \
+	pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils blueman tilix \
 	noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont ttf-liberation \
     	ttf-droid ttf-inconsolata ttf-roboto terminus-font ttf-font-awesome \
 	alsa-utils alsa-plugins alsa-lib pavucontrol lightdm lightdm-gtk-greeter 
