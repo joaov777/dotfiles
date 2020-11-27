d42e0b967c561e5c6472b4e16a2609584cf16650
joaov777 <joaov777@gmail.com>
2020-11-26 21:04:29 -0300

    Adding unison and font-manager to xfce packages

--- mainconftree/updatepackagesxfce.sh
+++ mainconftree/updatepackagesxfce.sh
@@ -30,7 +30,7 @@ packages_required=(
 	visual-studio-code-bin google-chrome bind-tools brave \
 	git tilda notepadqq gparted papirus-folders-git woeusb \
 	filezilla tilix telegram-desktop copyq flameshot ranger \
-	gedit pwgen vim rdesktop i3lock mtr tmux \
+	gedit pwgen vim rdesktop i3lock mtr tmux font-manager unison \
 	nmap okular viewnior ncdu inxi otf-fira-mono nordvpn-bin \
 	veracrypt papirus-icon-theme neofetch snes9x-gtk \
 	arp-scan net-tools teamviewer rclone peek krdc gvfs-smb thunar-shares-plugin-gtk3 gvfs 
