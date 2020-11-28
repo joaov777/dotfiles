6ddaeaabd03519641fad9f47d8d9fc8409a0fb48
joaov777 <joaov777@gmail.com>
2020-11-28 14:41:09 -0300

    Adding vifm to i3 packages

--- mainconftree/updatepackagesi3.sh
+++ mainconftree/updatepackagesi3.sh
@@ -32,7 +32,7 @@ packages_required=(
 	filezilla telegram-desktop copyq flameshot ranger pulseaudio-ctl speedtest-cli xfce4-power-manager\
 	gedit pwgen openssh vim rdesktop i3lock i3lock-fancy-git mtr tmux iw py3status \
 	nmap okular viewnior ncdu inxi htop otf-fira-mono nordvpn-bin nitrogen netctl \
-	veracrypt papirus-icon-theme neofetch lxappearance vlc picom i3status trayer \
+	veracrypt papirus-icon-theme neofetch lxappearance vlc picom i3status trayer vifm \
 	arp-scan net-tools teamviewer rclone peek xorg-server xorg-apps xorg-init networkmanager-dmenu-git \ 
 	imagewriter albert wget dnsutils xorg-xrandr arandr sshfs nm-connection-editor \ 
 	pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils blueman tilix \
