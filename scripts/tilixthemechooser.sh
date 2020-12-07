18e7fbe2dc317d26788dc8d01cb9495b9b2f0348
joaov777 <joaov777@gmail.com>
2020-12-07 10:23:53 -0300

    Improving tilix theme

--- scripts/tilixthemechooser.sh
+++ scripts/tilixthemechooser.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
   [[ "$#" -eq 0  ]] && {
-    echo "Insert a Tilix theme!"
+    ls ~/mainconf/varied/tilix
   } || {
-    case "$1" in
-      list|ll|l|--list) ls ~/mainconf/varied/tilix ;;
-      *) 
         [ $(ls ~/mainconf/varied/tilix | grep "$1") ] && {  
           dconf load /com/gexperts/Tilix/ < ~/mainconf/varied/tilix/$1.dconf
         } || {
           echo "Tilix Theme not defined!"
         }
-    esac
 
   }
