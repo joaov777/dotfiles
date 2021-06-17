4c935014266c0b7c3f5af4a5a93e5e73ebc625fa
joaov777 <joaov777@gmail.com>
2021-06-16 21:54:31 -0300

    Improving IFCE VPN script

--- scripts/ifc.sh
+++ scripts/ifc.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+	#ovpn keys directory - CHANGE WHEN NECESSARY
+	directory="/home/$USER/Documents/IFCERelated/VPN/"
+
 	#convert campus name to UPPERCASE
 	campus=`echo "$1" | awk '{ print toupper($0) }'`
+	
+	#list of openvpn PID's
+	openvpnpid="$(ps aux | grep $campus.ifce.ovpn | sed '$d' | awk '{print $2}')"
 
 	#If the pipe is already created, it breaks it. If not created yet, it will be so and vice-versa.
 	case $campus in
@@ -9,12 +15,25 @@
 	
 			if [ -z "$(ps aux | grep $campus.ifce.ovpn | sed '$d' | awk '{print $2}')" ]
 			then
-				sudo openvpn --config /home/$USER/mainconf/varied/ifcevpnkeys/joao.galvino$campus.ifce.ovpn --daemon && echo "VPN $campus pipe created"
+				sudo openvpn --config "$directory"joao.galvino"$campus".ifce.ovpn --daemon && echo "VPN $campus pipe created"
 			else
-				sudo kill -9 "$(ps aux | grep $campus.ifce.ovpn | sed '$d' | awk '{print $2}')" && echo "VPN $campus pipe destroyed"
+				sudo kill -9 "$(ps aux | grep "$campus".ifce.ovpn | sed '$d' | awk '{print $2}')" && echo "VPN $campus pipe destroyed"
+			fi ;;
+		
+		d|D|destroy|d|D|DESTROY)
+			if [ -z "$openvpnpid" ]
+			then
+				sudo killall openvpn && echo "No pipe established"
 			fi ;;
 	
-		*) echo "$(ps aux | grep $campus.ifce.ovpn | sed '$d')"
+		*) 
+			if [ -z "$openvpnpid" ]
+			then
+				echo "No pipe established"
+			else
+				ps aux | grep $campus.ifce.ovpn | sed '$d'
+			fi
+				
 	esac
 
 
