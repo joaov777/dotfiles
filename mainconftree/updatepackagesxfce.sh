e62c739a446cd088189b0585026840af59ca9de9
joaov777 <joaov777@gmail.com>
2021-06-19 21:09:41 -0300

    update

--- mainconftree/updatepackagesxfce.sh
+++ mainconftree/updatepackagesxfce.sh
@@ -23,6 +23,7 @@ function check_yay(){
 		git clone --quiet https://aur.archlinux.org/yay.git /tmp
 		cd /tmp/yay
 		makepkg -si
+	fi
 
 }
 	if [ -z "$(pacman -Qi yay)" ]; then sudo pacman -S yay --needed --noconfirm; else echo "|--> Yay is installed" && sleep 1; fi
