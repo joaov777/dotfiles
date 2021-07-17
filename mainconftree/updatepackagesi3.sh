5d85c3b59b4fc2badf0b198dd25223b2ee8ac742
joaov777 <joaov777@gmail.com>
2021-07-16 22:05:02 -0300

    Updating enableconfigfiles and adding openvpn to packages

--- mainconftree/updatepackagesi3.sh
+++ mainconftree/updatepackagesi3.sh
@@ -11,7 +11,7 @@ packages_required=(
 	arp-scan net-tools teamviewer rclone peek xorg-server xorg-apps xorg-init networkmanager-dmenu-git \ 
 	imagewriter albert wget dnsutils xorg-xrandr arandr sshfs nm-connection-editor nerd-fonts-fira-code \ 
 	pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils blueman tilix autorandr feh \
-	noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont ttf-liberation dialog polybar  \
+	noto-fonts ttf-ubuntu-font-family ttf-dejavu openvpn ttf-freefont ttf-liberation dialog polybar  \
   ttf-droid ttf-inconsolata ttf-roboto terminus-font ttf-font-awesome xournalpp font-awesome ttf-unifont siji-git \
 	alsa-utils alsa-plugins alsa-lib pavucontrol lightdm lightdm-gtk-greeter 
 	)
