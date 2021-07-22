a3b9fb7ccfdf95aab959a9c719d5748d7ee4c878
joaov777 <joaov777@gmail.com>
2021-07-21 21:53:31 -0300

    Fixed mainconf.sh with simpler method

--- mainconf.sh
+++ mainconf.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-    if [ $XDG_CURRENT_DESKTOP == "XFCE" ];
-    then
+    ~/mainconf/mainconf"$1".sh
 
-        ~/mainconf/mainconfxfce.sh
-
-    elif [ $XDG_CURRENT_DESKTOP == "i3" ];
-        then
-        
-        ~/mainconf/mainconfi3.sh
-
-    else
-        echo "DE not configured!"
-    fi
