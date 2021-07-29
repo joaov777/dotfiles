0ae0f12ec8b62de09219f8ae60f4c31c99366eeb
joaov777 <joaov777@gmail.com>
2021-07-29 20:55:35 -0300

    Updating polybar config files for automatic multimonitor support

--- varied/i3related/polybar/launch_polybar.sh
+++ varied/i3related/polybar/launch_polybar.sh
@@ -5,12 +5,22 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bottom_right &
+    #MONITOR=$m polybar bottom_right 2>&1 | tee -a /tmp/polybar1.log & disown
+  done
+else
+  polybar --reload bottom_right &
+fi
+
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 #polybar top_right 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bottom_right 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar bottom_right 2>&1 | tee -a /tmp/polybar1.log & disown
 #polybar top_left 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bottom_left 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar bottom_left 2>&1 | tee -a /tmp/polybar1.log & disown
 
 
 
