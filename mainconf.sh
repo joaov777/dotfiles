64afd922695762142db1b6bdc2d234e83a7684ff
Joao <joao@DGTI-556133RE>
2021-09-28 19:54:24 -0400

    Improving mainconf main menu

--- mainconf.sh
+++ mainconf.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+. ~/mainconf/scripts/menus.sh
 
-    ~/mainconf/mainconf"$1".sh
+while [ true ];
+        do
 
+        menu "MAINCONF - "
+        echo "#(1) - i3"
+        echo "#(2) - xfce"
+        echo "|-------------------------------------------|"
+        read -p "Option: " menuChoice
+        
+
+        case $menuChoice in
+                q|Q|quit|QUIT|Quit|qUIT|exit|EXIT|Exit) exit ;;
+                1) ~/mainconf/mainconfi3.sh ;;
+                2) ~/mainconf/mainconfxfce.sh ;;
+                *) echo "Not defined!" && sleep 1 ;;
+        esac
+
+        done ##Menu Boundary
