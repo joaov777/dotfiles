6b17cffa5a289b00c2f5c35153c2a6f0217b9188
Joao <39555841+joaov777@users.noreply.github.com>
2022-04-10 16:09:48 -0300

    Update updatepackagesi3.sh

--- mainconftree/updatepackagesi3.sh
+++ mainconftree/updatepackagesi3.sh
@@ -1,12 +1,9 @@
+
 #!/bin/bash
+. ~/mainconf/scripts/menus.sh
 
-#Including menus file
-installpath=~/mainconf/scripts/
-. $installpath/menus.sh
-
-#list of available packages
-available_packages=(
-    trizen ly i3-gaps pdfarranger openssh tcpdump tldr fzf thunar termite rclone \
+packages_required=(
+	ly i3 pdfarranger openssh tcpdump tldr fzf thunar termite rclone \
 	visual-studio-code-bin bind-tools rofi dmenu brave keepassxc brightnessctl netctl \
 	git tilda notepadqq gparted papirus-folders-git acpi pulseaudio-alsa xfce4-notifyd \
 	filezilla telegram-desktop copyq flameshot ranger pulseaudio-ctl xfce4-power-manager\
@@ -14,113 +11,87 @@ available_packages=(
 	nmap okular viewnior ncdu inxi htop otf-fira-mono nordvpn-bin nitrogen netctl \
 	veracrypt papirus-icon-theme neofetch lxappearance vlc picom i3status trayer vifm exa \
 	arp-scan net-tools teamviewer peek xorg-server xorg-apps xorg-init networkmanager-dmenu-git \ 
-	imagewriter albert wget dnsutils xorg-xrandr arandr sshfs nm-connection-editor nerd-fonts-fira-code \ 
+	imagewriter wget dnsutils xorg-xrandr arandr sshfs nm-connection-editor nerd-fonts-fira-code \ 
 	pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils blueman tilix autorandr feh \
 	noto-fonts ttf-ubuntu-font-family ttf-dejavu openvpn ttf-freefont ttf-liberation dialog polybar  \
-  	ttf-droid ttf-inconsolata ttf-roboto terminus-font ttf-font-awesome xournalpp font-awesome ttf-unifont siji-git \
-	alsa-utils alsa-plugins alsa-lib pavucontrol zathura zathura-pdf-mupdf 
-)
+  	ttf-droid ttf-inconsolata ttf-roboto terminus-font ttf-font-awesome ttf-font-awesome-5 otf-font-awesome \
+	xournalpp font-awesome ttf-unifont siji-git \
+	alsa-utils alsa-plugins alsa-lib pavucontrol zathura zathura-pdf-mupdf  
+	)
 
-#FUNCTION - display available packages
-function showAvailablePackages() {
-    counter=0
-
-    for i in "${!available_packages[@]}"; do 
-        
-        #display package and its equivalent number 
-        echo -n "${i}. ${available_packages[i]}  "
-
-        #print 10 packages per line
-        [ "$counter" == 9 ] && {
-            echo && counter=0
-        } || {
-            counter=$((counter+1))
-        }
-    done
-            echo ""
+function check_installation() {
+	if [ -z "$(pacman -Qi $1)" ]; then echo "|--> ERROR: Package $1" && sleep 1; else echo "|--> SUCCESS: Package $1" && sleep 1; fi
 }
-
-#FUNCTION isInstalled - Check whether a package is installed or not
-function isInstalled() {
-    [ ! -z "$package" ] && {
-        echo "|--> Package $package installed!" && sleep 2
-    } || {
-        echo "|--> Package $package not installed!" && sleep 2
-    }
+	
+#checking Yay AUR Helper
+function check_yay(){
+		git clone --quiet https://aur.archlinux.org/yay.git 
+		cd yay
+		makepkg -si
+		sudo rm -rf yay
 }
+	#if [ -z "$(pacman -Qi yay)" ]; then sudo pacman -S yay --needed --noconfirm; else echo "|--> Yay is installed" && sleep 1; fi
+	check_yay
+	   
+for pkg in "${packages_required[@]}"; do
+	clear
+	echo "$(tput bold)$(tput setaf 7)"
+	echo "|==============|> MAINCONF <|==============|"
+	echo "|------------> UPDATE PACKAGES <-----------|"
 
-#FUNCTION checkYay - Checks if helper Yay is installed
-function checkYay() {
-	[ -z $(pacman -Qi yay) ] && {
-		echo "|--> Installling Yay AUR Helper..."
-		
-		#creates the temporary directory
-		mkdir -p ~/yay && 
+	#subMenu "MAINCONF - i3" "Update Packages XFCE"
 
-		#clones yay helper
-		git clone https://aur.archlinux.org/yay.git ~/yay --quiet && 
+	#manage installation
+	if [ -z "$(pacman -Qi $pkg)" ]; then 
+		echo "|--> Installing $pkg..." 
+		yay -S "$pkg" --noconfirm --needed 
+		check_installation "$pkg"
+	else
+		echo "|--> $pkg is already installed!!" > /dev/null 
+	fi
+done
 
-		#installs helper silently
-		cd ~/yay && makepkg -si && 
-
-		#feedback for user
-		echo "|--> Yay installed" && sleep 1 
-	}
-
-	#removing temporary yay folder
-	[ -d ~/yay ] && rm -rf ~/yay 
-}
-
-#FUNCTION installPackages - Install or uninstall chosen packages
-function installPackages() {
-
-	subMenu "MAINCONF - i3" "Update Packages"
-    showAvailablePackages
-
-	#save the chosen packages as arguments into an array
-    read -p "Packages: " -a chosen_packages 
-
-    for pkg in "${!chosen_packages[@]}"; do
-        package="${available_packages[${chosen_packages[$pkg]}]}" 
-		yay -S "$package" --noconfirm --needed &> /dev/null
-        isInstalled
-    done
-}
-	checkYay
-	installPackages 
-
-	#for Thunar
-	sudo pacman -S gvfs gvfs-smb gvfs-mtp --noconfirm --needed &> /dev/null
-	#sudo smb passwd -a joao
+	#enabling sharing
+	echo "|--> Enabling Samba permissions" ; 
+	sudo smbpasswd -a $USER 
+	papirus-folders -C white > /dev/null
+	xdg-user-dirs-update
 
 	#enabling some settings for vim
-	cp ~/mainconf/varied/vifm/.vimrc /home/$USER
+	cp ~/mainconf/varied/vifm/.vimrc ~
 
 	#installing cursor themes
+	echo "|--> Installing Cursor Themes" ; 
 	sudo cp -r ~/mainconf/varied/cursors/* /usr/share/icons/
 
-	#enabling bluetooth service
-	sudo systemctl enable bluetooth --now
-
 	#installing themes
+	echo "|--> Installing Themes" ; 
 	sudo cp -r ~/mainconf/varied/themes/* /usr/share/themes/
 
 	#teamviewer related
-	sudo teamviewer --daemon enable &> /dev/null
-	sudo systemctl enable teamviewerd.service 
-	sudo systemctl start teamviewerd.service 
+	echo "|--> Enabling TeamViewer" ; 
+	sudo teamviewer --daemon enable > /dev/null
+	sudo systemctl enable teamviewerd.service --now > /dev/null
 
 	#tmux related
+	echo "|--> Setting up TMUX" ; 
 	cp ~/mainconf/varied/tmux/.tmux.conf ~
+	
+	#notepadqq
+	echo "|--> Setting up Notepadqq" ; 
+	notepadqq=~/.config/Notepadqq
+    [ -f $notepadqq/Notepadqq.ini ] && rm $notepadqq/Notepadqq.ini || mkdir -p $notepadqq
+    cp ~/mainconf/varied/notepadqq/Notepadqq.ini $notepadqq && ln -sf ~/mainconf/varied/notepadqq/Notepadqq.ini $notepadqq
+
+	#tilix
+	echo "|--> Setting up Tilix" ; 
+	dconf load /com/gexperts/Tilix/ < ~/mainconf/varied/tilix/tilix.dconf
 
 	#nord related
+	echo "|--> Enabling and Starting VPN service" ; 
 	sudo systemctl enable nordvpnd --now
-
-	#printing related - install manually on the 'print settings' GUI
-	#sudo pacman -S manjaro-printer
-	#sudo gpasswd $USER sys
-	#sudo systemctl enable --now org.cups.cupsd.service
-
-	sudo papirus-folders -C black &> /dev/null
-
+	
     clear
+
+
+
