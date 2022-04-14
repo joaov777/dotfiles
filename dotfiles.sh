b3bc0e00bf5434de1d04844764cd0d5e25e8b545
joaov777 <joao@ironhide.localdomain>
2022-04-14 18:02:27 -0300

    updating dotfiles.sh script

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,17 +3,14 @@
 while [ true ];
         do
 
-        
-        if [ $# -eq 0 ]; then
-                echo -e "Usage:\n$0 i3\n$0 xfce" && exit
-        elif [ "$1" == "i3" ]; then 
+        if [ "$1" == "i3" ]; then 
                 exec ~/dotfiles/scripts/menutree/dotfilesi3.sh
         elif [ "$1" == "xfce" ]; then 
                 exec ~/dotfiles/scripts/menutree/dotfilesxfce.sh
         elif [ "$1" == "q" ]; then 
                 exit
         else
-                echo "Not defined!" && sleep 1
+                echo -e "Usage:\n$0 i3\n$0 xfce" && exit
         fi
 
         done ##Menu Boundary
