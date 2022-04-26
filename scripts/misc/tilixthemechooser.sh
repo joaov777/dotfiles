a3fca4ae087d067efe29c67a01c3c268273e517a
joaov777 <joaov777@gmail.com>
2022-04-26 08:15:09 -0300

    updating tt tilix theme chooser script

--- scripts/misc/tilixthemechooser.sh
+++ scripts/misc/tilixthemechooser.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
   [[ "$#" -eq 0  ]] && {
-    ls ~/mainconf/varied/tilix
+    ls ~/dotfiles/config/tilix
   } || {
-        [ $(ls ~/mainconf/varied/tilix | grep "$1") ] && {  
+        [ $(ls ~/dotfiles/config/tilix | grep "$1") ] && {  
           dconf load /com/gexperts/Tilix/ < ~/mainconf/varied/tilix/$1.dconf
         } || {
           echo "Tilix Theme not defined!"
