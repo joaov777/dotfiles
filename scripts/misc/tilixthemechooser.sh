75322577d41068117627cba3481e37d9e4409317
joaov777 <joaov777@gmail.com>
2022-04-26 08:19:11 -0300

    updating tt script

--- scripts/misc/tilixthemechooser.sh
+++ scripts/misc/tilixthemechooser.sh
@@ -4,7 +4,7 @@
     ls ~/dotfiles/config/tilix
   } || {
         [ $(ls ~/dotfiles/config/tilix | grep "$1") ] && {  
-          dconf load /com/gexperts/Tilix/ < ~/mainconf/varied/tilix/$1.dconf
+          dconf load /com/gexperts/Tilix/ < ~/dotfiles/config/tilix/$1.dconf
         } || {
           echo "Tilix Theme not defined!"
         }
