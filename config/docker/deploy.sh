ecf10a08771dcc4a0c947a78c830e40fa014baba
joaov777 <joaov777@gmail.com>
2022-07-20 22:08:04 -0300

    updating docker related files

---
+++ config/docker/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_FILE=$(readlink -f "$0") # script absolute path
+SCRIPT_DIR=$(dirname "$SCRIPT_FILE") # script directory
+PROJECT_DIR=$(dirname $(dirname $SCRIPT_DIR)) #
+CONTAINER_NAME="dotfiles_container"
+IMAGE_NAME="dotfiles_image"
+
+# Uncomment this line for debugging
+echo -e "$SCRIPT_FILE\n$SCRIPT_DIR\n$PROJECT_DIR\n$CONTAINER_NAME" 
+
+# retrieve all local container names
+CONTAINER=$(docker ps -f name=$CONTAINER_NAME --format "{{.Names}}")
+echo $CONTAINER && sleep 10
+
+# delete potential existing container
+[ $CONTAINER ] && docker rm -f $CONTAINER_NAME
+
+echo "--- Building image..."
+docker build -t "$IMAGE_NAME" -f Dockerfile .
+
+echo "--- Building container..."
+docker run --rm -it --name "$CONTAINER_NAME" -v $(pwd):/home/joao/dotfiles "$IMAGE_NAME"
+
