36f99e22c00b2c148ede5f7cc03f49b9f50528cc
joaov777 <joaov777@gmail.com>
2023-08-16 20:58:54 -0300

    adjusting font installation

--- scripts/install_system_fonts.sh
+++ scripts/install_system_fonts.sh
@@ -7,17 +7,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$SCRIPT_DIR"/../scripts/menus.sh
 . "$SCRIPT_DIR"/../scripts/functions.sh
 
-FONTS_DIR="~/.local/share/fonts"
+FONTS_DIR="$HOME/.local/share/fonts"
         
         subMenu "Dotfiles" "Install System Fonts"
 
 		[ -d "$FONTS_DIR" ] && rm -rf "$FONTS_DIR" 
 		mkdir -p "$FONTS_DIR"
 
-		cp "$HOME"/dotfiles/config/systemfonts/consolas/con* "$FONTS_DIR"
-		cp "$HOME"/dotfiles/config/systemfonts/jetbrains/Jet* "$FONTS_DIR"
-		cp "$HOME"/dotfiles/config/systemfonts/noto/Noto* "$FONTS_DIR"
-		cp "$HOME"/dotfiles/config/systemfonts/fonteawesome5/Font* "$FONTS_DIR"
-		cp "$HOME"/dotfiles/config/systemfonts/siji/sij* "$FONTS_DIR"
+		cp -r "$SCRIPT_DIR"/../config/systemfonts/consolas/ "$FONTS_DIR"
+		cp -r "$SCRIPT_DIR"/../config/systemfonts/jetbrains/ "$FONTS_DIR"
+		cp -r "$SCRIPT_DIR"/../config/systemfonts/noto/ "$FONTS_DIR"
+		cp -r "$SCRIPT_DIR"/../config/systemfonts/fontawesome5/ "$FONTS_DIR"
+		cp -r "$SCRIPT_DIR"/../config/systemfonts/siji/ "$FONTS_DIR"
 
 		fc-cache -f -v
