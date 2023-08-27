ae088e3de8f9526f2e6e77845440df1cd628b525
joaov777 <joaov777@gmail.com>
2023-08-27 15:47:31 -0300

    minor adjustment on yay helper installation script

--- scripts/install_yay_helper.sh
+++ scripts/install_yay_helper.sh
@@ -14,17 +14,19 @@ subMenu "Dotfiles" "Install AUR Helper"
     # if the package is not installed
     if ! isPackageInstalled yay; then
 
-            echo "|--> Yay has not been installed yet!" ; sleep 3
-            [ -d "$yay_dir" ] && sudo rm -rf "$yay_dir"
-            sudo pacman -S --needed --noconfirm git base-devel && \
-            git clone --quiet https://aur.archlinux.org/yay.git "$yay_dir" && cd "$yay_dir" && \
-            makepkg -si && cd .. && \
-            rm -rf "$yay_dir"
+        echo "|--> Yay has not been installed yet!" ; sleep 2
+        [ -d "$yay_dir" ] && sudo rm -rf "$yay_dir"
+        sudo pacman -S --needed --noconfirm git base-devel && \
+        git clone --quiet https://aur.archlinux.org/yay.git "$yay_dir" && cd "$yay_dir" && \
+        makepkg -si && cd .. && \
+        rm -rf "$yay_dir"
 
-            # checking whether the package was successfully installed
-            if isPackageInstalled yay; then
-                echo "|--> Yay successfully installed!" ; sleep 3
-            else
-                echo "|--> Yay not installed!" ; sleep 3
-            fi
+        # checking whether the package was successfully installed
+        if isPackageInstalled yay; then
+            echo "|--> Yay successfully installed!" ; sleep 2
+        else
+            echo "|--> Yay not installed!" ; sleep 2
+        fi
+    else    
+        echo "|--> Yay has already been installed!" ; sleep 2
 	fi
