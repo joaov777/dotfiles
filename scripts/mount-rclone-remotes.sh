ff09d198736e32cb81b9a8813315ba6f61394837
joaov777 <joaov777@gmail.com>
2023-10-18 10:41:26 -0300

    major changes on rclone script

--- scripts/mount-rclone-remotes.sh
+++ scripts/mount-rclone-remotes.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# This script mounts rclone remotes automatically 
-# mount-rclone-remotes.sh mount --all
-#
+# This script mounts/umounts rclone remotes 
+# mount-rclone-remotes.sh --mount --all
+# mount-rclone-remotes.sh --umount --all
+# mount-rclone-remotes.sh -mount <remote>
+# mount-rclone-remotes.sh --umount <remote>
 
-# # Define the list of remotes
-# REMOTES=("remote1" "remote2" "remote3")
-
-# # Mount each remote
-# for remote in "${REMOTES[@]}"; do
-#     $RCLONE_PATH mount $remote: /path/to/mount/point/$remote &
-# done
-
+# variables
 RCLONE_PATH=$(which rclone)
 MANAGED_DIR="/tmp"
 LIST_RCLONE="$RCLONE_PATH listremotes"
 
-# Save the list of remotes as an array
+# list of remotes
 REMOTES=("ifce" "ifceshared" "joao")
 
 
-[ "$1" == "list" ] && { 
-    echo "Available remotes:"
-    eval "$LIST_RCLONE" 
-    exit 0
+show_usage_missing_mount_name() {
+	echo "Error! Missing mount name."
+    echo "Usage: $0 --mount/--umount <mount_name> or $0 --mount/--umount --all"
+    exit 1
 }
 
 case "$1" in
-    "mount") 
+	"--list")
+		echo "Available remotes:"
+		eval "$LIST_RCLONE" 
+		exit 0
+	;;
+    "--mount") 
         if [ "$2" == "--all" ]; then
             for remote in "${REMOTES[@]}"; do
 				mkdir -p "$MANAGED_DIR/$remote"
@@ -37,33 +37,30 @@ case "$1" in
             done
         else
             [ -z "$2" ] && {
-                echo "Error! Missing mount name."
-                echo "Usage: $0 mount/umount <mount_name> or $0 mount --all"
-                exit 1
+                show_usage_missing_mount_name && exit 1
             }
             [ ! -d "$MANAGED_DIR/$2" ] && mkdir -p "$MANAGED_DIR/$2"
             nohup $RCLONE_PATH mount $2: $MANAGED_DIR/$2 --daemon >/dev/null 2>&1 &
             echo "Mounted remote $2."
         fi
         ;; 
-    "umount") 
+    "--umount") 
         if [ "$2" == "--all" ]; then
 			for remote in "${REMOTES[@]}"; do
-				fusermount -u "$MANAGED_DIR"/$remote && rm -rf "$MANAGED_DIR"/$remote
-            	echo "Unmounted all remotes."
+				fusermount -u "$MANAGED_DIR"/$remote && rm -rf "$MANAGED_DIR"/$remote 
+				sleep 1
 			done
+			[ $? -eq 0 ] && echo "--> Success! Unmounted all remotes" || echo "--> Error! Not all remotes unmounted"
         else
             [ -z "$2" ] && {
-                echo "Error! Missing mount name."
-                echo "Usage: $0 mount/umount <mount_name> or $0 umount --all"
-                exit 1
+                show_usage_missing_mount_name && exit 1
             }
-            fusermount -u $MANAGED_DIR/$2 && rm -rf "$MANAGED_DIR/$2"
+            fusermount -u $MANAGED_DIR/$2 >/dev/null 2>&1 && rm -rf "$MANAGED_DIR/$2" >/dev/null 2>&1
             echo "Unmounted remote $2."
         fi
         ;; 
     *) 
-        echo "-- Invalid option! Use 'mount' or 'umount'" 
+        echo "-- Invalid option! Use 'mount', 'umount' or 'list'" 
         ;;
 esac
 
